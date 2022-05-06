d7121bdcdaa59b81970af7df6449bae9672f88e3
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.fritz.box>
2022-05-06 16:59:06 +0200

    TimeLock updated

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -2,8 +2,12 @@
 
 // Time controler of Governance
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-contract TimeLock{
+import "@openzeppelin/contracts/governance/TimelockController.sol";
 
+contract TimeLock is TimelockController {
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors) TimelockController(minDelay, proposers, executors) {
+        
+    }
 }
