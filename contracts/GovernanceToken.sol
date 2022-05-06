45adae81e6ba3cc214e1423835656f9e30dc4056
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.fritz.box>
2022-05-06 15:16:20 +0200

    compiled

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -17,4 +17,22 @@ contract GovernanceToken is ERC20Votes {
         _mint(msg.sender, s_maxSupply);
     }
 
+    /**
+    * @notice The functions below are overrides required by Solidity
+    * @dev Function to update states after transfer of funds
+    */
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    /// @dev Function to mint tokens: increases amount of tokens in circulation
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+     /// @dev Function to burn tokens: decreases amount of tokens in circulation
+    function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+        super._burn(account, amount);
+    }
+
 }
