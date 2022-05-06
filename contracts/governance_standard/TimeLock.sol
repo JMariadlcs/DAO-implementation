b5f18d3d4d156fc9b75fefdc2d2b4ff9df2317a6
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.fritz.box>
2022-05-06 17:01:24 +0200

    update

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -7,7 +7,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
+
+     /**
+    * @dev
+    * - proposers: address that can propose a new proposal
+    * - executors: address that can execute a new proposal once time is over
+    */
     constructor(uint256 minDelay, address[] memory proposers, address[] memory executors) TimelockController(minDelay, proposers, executors) {
-        
+
     }
 }
