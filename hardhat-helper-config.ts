33c4820575b587a49aa633f34b8fd73b61d5e821
Chema De la Cruz Sanchez <chemadelacruzsanchez@MacBook-Pro-de-Chema-2.fritz.box>
2022-05-07 00:58:11 +0200

    address zero added

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -6,4 +6,4 @@ export const MIN_DELAY = 3600;
 export const PROPOSERS: string[] = [];
 export const EXECUTORS: string[] = [];
 
-export const ADDRESS_ZERO = "";
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000" // no address in particular -> everybody
