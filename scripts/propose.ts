d649f136df7456514e046af766baf2af416cd56d
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-05-07 13:37:25 +0200

    propose script

---
+++ scripts/propose.ts
@@ -0,0 +1,32 @@
+// SCRIPT USED TO MAKE A PROPOSAL
+
+// PARAMETERS FROM PROPOSE FUNCTION IN GovernorContract.sol:
+// address[] memory targets -> Box.sol address (array of contracts)
+// uint256[] memory values -> Ethers sent
+// bytes[] memory calldatas -> The funciton we are calling
+// string memory description -> What is being proposed
+
+import { ethers } from "hardhat"
+
+export async function makeProposal(functionToCall: string, args: number[], proposalDescription: string) {
+
+    // Import needed contracts
+   const governor = await ethers.getContract("GovernorContract")
+   const box = await ethers.getContract("Box")
+
+   const encodedFunctionCall = box.interface.encodeFunctionData( // use to encode the funciton we are executing in the propose()
+       functionToCall,
+       args
+   );
+
+   // We call to propose function in GovernorContract.sol
+   const proposeTx = await governor.propose(
+       [box.address],
+       [0],
+       [encodedFunctionCall],
+       proposalDescription
+   )
+
+   const proposeReceipt = proposeTx.wait(1)
+   
+};
