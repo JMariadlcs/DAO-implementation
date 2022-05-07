d516d400e16acfe12c189b2243b4b09d9695bd2f
Chema De la Cruz Sanchez <chemadelacruzsanchez@MBP-de-Chema-2.fritz.box>
2022-05-07 14:10:54 +0200

    moveBlocks helper

--- helpers.ts
+++ helpers.ts
@@ -1,1 +1,11 @@
-export const moveBlocks = async()
+import { network } from "hardhat"
+
+export const moveBlocks = async( amount: number ) => {
+    for (let i = 0; i < amount; i++) {
+        await network.provider.request({
+            method: "evm_mine",
+            params: [],
+        });
+    }
+    console.log(`Moved ${amount} blocks`);
+}
